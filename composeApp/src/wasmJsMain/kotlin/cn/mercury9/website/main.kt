34f6797aadf9c9baa95f578f4ab1647506789ceb
MercuryGryph <w84627302@gmail.com>
2024-11-21 20:31:50 +0800

    添加wasm不兼容提示

--- composeApp/src/wasmJsMain/kotlin/cn/mercury9/website/main.kt
+++ composeApp/src/wasmJsMain/kotlin/cn/mercury9/website/main.kt
@@ -3,9 +3,11 @@ package cn.mercury9.website
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.dom.clear
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+  document.body!!.clear()
   ComposeViewport(document.body!!) {
     App()
   }
